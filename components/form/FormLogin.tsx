313d5ff4f3ec92caf4e24e82496a0ba280588c33
trieunguyentm <trieunguyen241102@gmail.com>
2024-03-31 05:26:55 +0700

    update: middleware

--- components/form/FormLogin.tsx
+++ components/form/FormLogin.tsx
@@ -2,12 +2,33 @@
 
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
-import { ArrowRight, Eye, EyeOff, KeyRound, User } from "lucide-react"
+import { ArrowRight, CircleAlert, Eye, EyeOff, KeyRound, Loader2, User } from "lucide-react"
 import { FormEvent, useEffect, useState } from "react"
 import Link from "next/link"
 import SnackbarCustom from "../ui/snackbar"
+import { Tooltip, Zoom } from "@mui/material"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/navigation"
+
+type FormValues = {
+    email: string
+    password: string
+}
 
 const FormLogin = () => {
+    const router = useRouter()
+    const [loading, setLoading] = useState<boolean>(false)
+    /** Hidden or Show password */
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+    /** React hook form */
+    const {
+        register,
+        watch,
+        formState: { errors },
+        handleSubmit,
+    } = useForm<FormValues>({
+        mode: "onChange",
+    })
     /** Snack Bar */
     const [openSnackbar, setOpenSnackbar] = useState<boolean>(false)
     const [typeSnackbar, setTypeSnackbar] = useState<"success" | "info" | "warning" | "error">(
@@ -30,51 +51,133 @@ const FormLogin = () => {
         }
     }, [])
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const formData = new FormData(e.currentTarget)
-
-        console.log(formData.get("email"))
-        console.log(formData.get("password"))
+    const handleSubmitForm = async () => {
+        setLoading(true)
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+                body: JSON.stringify({
+                    email: watch("email"),
+                    password: watch("password"),
+                }),
+            })
+            const data = await res.json()
+            console.log(data)
+            if (!res.ok) {
+                setOpenSnackbar(true)
+                setTypeSnackbar("error")
+                setContentSnackbar(data.message)
+            }
+            if (data.success) {
+                router.push("/")
+            }
+        } catch (error) {
+            console.log(error)
+            setOpenSnackbar(true)
+            setTypeSnackbar("error")
+            setContentSnackbar("An error occurred, please try again")
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
-        <form onSubmit={handleSubmit} className="mt-10 flex flex-col justify-center gap-5">
+        <form
+            onSubmit={handleSubmit(handleSubmitForm)}
+            className="mt-10 flex flex-col justify-center gap-5"
+        >
             <div className="flex items-center gap-3">
                 <label htmlFor="email">
                     <User className="w-10 h-10 text-brown-1 max-md:w-8 max-md:h-8" />
                 </label>
                 <input
+                    {...register("email", {
+                        required: "Email is required",
+                        validate: (value) => {
+                            const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+                            if (regex.test(value)) {
+                                return true
+                            } else return "Invalid email"
+                        },
+                    })}
                     type="text"
                     name="email"
                     id="email"
                     placeholder="Enter your email"
                     className="flex-1 p-4 max-sm:p-2 pr-16 max-sm:pr-8 border-brown-1 rounded-2xl border-2 text-lg font-normal focus:outline-none"
                 />
+                <Tooltip
+                    TransitionComponent={Zoom}
+                    title={<div className="text-red-500 p-0">{errors.email?.message}</div>}
+                    placement="top"
+                >
+                    <CircleAlert
+                        className={`text-red-500 w-5 h-5 ${!errors.email && "invisible"}`}
+                    />
+                </Tooltip>
             </div>
             <div className="flex items-center gap-3 relative">
                 <label htmlFor="password">
                     <KeyRound className="w-10 h-10 text-brown-1 max-md:w-8 max-md:h-8" />
                 </label>
                 <input
-                    type="password"
+                    {...register("password", {
+                        required: "Password is required",
+                        validate: (value: string) => {
+                            if (value.trim().length < 6) {
+                                return "Password must be at least 6 characters"
+                            }
+                        },
+                    })}
+                    autoComplete=""
+                    type={showPassword ? "text" : "password"}
                     name="password"
                     id="password"
                     placeholder="Enter your password"
                     className="flex-1 p-4 max-sm:p-2 pr-16 max-sm:pr-8 border-brown-1 rounded-2xl border-2 text-lg font-normal focus:outline-none"
                 />
-                <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-                    <EyeOff className="text-brown-1 w-8 h-8 max-md:w-5 max-md:h-5" />
+                <div className="absolute inset-y-0 right-0 mr-10 flex items-center text-sm leading-5">
+                    {!showPassword ? (
+                        <EyeOff
+                            onClick={() => setShowPassword((prev) => !prev)}
+                            className="text-brown-1 w-8 h-8 max-md:w-5 max-md:h-5 cursor-pointer"
+                        />
+                    ) : (
+                        <Eye
+                            onClick={() => setShowPassword((prev) => !prev)}
+                            className="text-brown-1 w-8 h-8 max-md:w-5 max-md:h-5 cursor-pointer"
+                        />
+                    )}
                 </div>
+                <Tooltip
+                    TransitionComponent={Zoom}
+                    title={<div className="text-red-500 p-0">{errors.password?.message}</div>}
+                    placement="top"
+                >
+                    <CircleAlert
+                        className={`text-red-500 w-5 h-5 ${!errors.password && "invisible"}`}
+                    />
+                </Tooltip>
             </div>
             <div className="flex items-cente justify-center">
                 <Button
+                    disabled={loading}
                     type="submit"
                     variant={"outline"}
                     className="border-brown-1 border-2 rounded-2xl text-brown-1 text-2xl max-md:text-xl py-5 px-6 flex items-center hover:text-brown-1"
                 >
-                    Login
-                    <ArrowRight />
+                    {loading ? (
+                        <Loader2 className="w-9 h-9 animate-spin" />
+                    ) : (
+                        <>
+                            Login
+                            <ArrowRight />
+                        </>
+                    )}
                 </Button>
             </div>
             <Link
---
+++ middleware.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from "next/server"
+import { NextRequest } from "next/server"
+
+const authRoute = [
+    "/login",
+    "/register",
+    "/verify-otp",
+    "/recovery-password",
+    "/verify-otp-recovery",
+    "/confirm-password",
+]
+
+const verifyRoute = ["/verify-otp", "/verify-otp-recovery", "/confirm-password"]
+
+export async function middleware(request: NextRequest) {
+    /** Set auth */
+    let auth: boolean = false
+    let cookie = request.cookies.get("refresh-token-id")
+    if (!cookie) {
+        auth = false
+    } else {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Cookie: `refresh-token-id=${cookie.value}`,
+            },
+            credentials: "include",
+        })
+        const data = await res.json()
+        if (data.auth) {
+            auth = true
+        }
+    }
+    /** Protect route */
+    let url: string = request.nextUrl.pathname
+    if (authRoute.includes(url) && auth) {
+        // Quay về trang chủ
+        return NextResponse.redirect(new URL("/", request.url))
+    }
+    if (authRoute.includes(url) && !auth) {
+        // Xử lý các route /verify-otp, /verify-otp-password, /confirm-password
+        if (verifyRoute.includes(url)) {
+            const cookieName = url.substring(1)
+            const cookieValue = request.cookies.get(cookieName)?.value
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/verify`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: `${cookieName}=${cookieValue}`,
+                },
+                credentials: "include",
+                body: JSON.stringify({
+                    cookieName,
+                }),
+            })
+            const data = await res.json()
+            if (!data.verify) {
+                return NextResponse.redirect(new URL("/login", request.url))
+            } else {
+                return NextResponse.next()
+            }
+        } else {
+            // Accept Request
+            return NextResponse.next()
+        }
+    }
+    if (!authRoute.includes(url) && !auth) {
+        // Quay về login
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+    if (!authRoute.includes(url) && auth) {
+        return NextResponse.next()
+    }
+}
+
+// Apply middleware to paths
+export const config = {
+    matcher: ["/((?!api|_next/static|favicon.ico|assets).*)"],
+}
