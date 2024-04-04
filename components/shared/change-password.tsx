e1d36df8255d41e1e534d8718191fd40643425ec
trieunguyentm <trieunguyen241102@gmail.com>
2024-04-04 21:23:57 +0700

    fix: metadata

---
+++ components/shared/change-password.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { useState } from "react"
+import { Button } from "../ui/button"
+import Image from "next/image"
+
+const ChangePassword = () => {
+    const [openChangePassword, setOpenChangePassword] = useState<boolean>(false)
+
+    return (
+        <>
+            <div className="flex flex-col">
+                <div className="border-b py-2 border-brown-1 font-semibold text-2xl text-brown-1">
+                    Account
+                </div>
+                <div className="py-5 flex flex-col text-brown-1 gap-2">
+                    <div className="flex items-center">
+                        <div className="w-1/6 font-medium">Username</div>
+                        <div className="w-5/6 text-sm">trieunguyentm</div>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="w-1/6 font-medium">Gmail</div>
+                        <div className="w-5/6 text-sm">trieunguyentm</div>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="w-1/6 font-medium">Password</div>
+                        <div className="w-5/6 text-sm flex gap-4">
+                            <div>*************</div>
+                            <button onClick={() => setOpenChangePassword((prev) => !prev)}>
+                                <Image
+                                    src={"/assets/images/pen-line.svg"}
+                                    alt="pen-line"
+                                    width={20}
+                                    height={20}
+                                />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {openChangePassword && (
+                <form className="flex flex-col gap-3 text-brown-1">
+                    <label htmlFor="password" className="font-medium">
+                        Current password
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        className="py-3 px-4 bg-pink-1 rounded-xl focus:outline-none max-w-[300px] border"
+                    />
+                    <label htmlFor="newPassword" className="font-medium">
+                        New password
+                    </label>
+                    <input
+                        type="password"
+                        id="newPassword"
+                        className="py-3 px-4 bg-pink-1 rounded-xl focus:outline-none max-w-[300px] border"
+                    />
+                    <label htmlFor="confirmPassword" className="font-medium">
+                        Confirm new password
+                    </label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        className="py-3 px-4 bg-pink-1 rounded-xl focus:outline-none max-w-[300px] border"
+                    />
+                    <div className="flex flex-row-reverse gap-3">
+                        <Button type="submit">Change password</Button>
+                        <Button
+                            variant="ghost"
+                            onClick={() => setOpenChangePassword((prev) => !prev)}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </form>
+            )}
+        </>
+    )
+}
+
+export default ChangePassword
